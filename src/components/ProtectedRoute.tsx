4970d2d628203e52120730fd963e987c12d31a4c
anne67j-boop <anne67j@gmail.com>
2025-12-15 13:04:47 +0100

    Create ProtectedRoute.tsx

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+
+function ProtectedRoute({ children }: { children: JSX.Element }) {
+  const isAuthenticated = localStorage.getItem('auth') === 'true'
+  return isAuthenticated ? children : <Navigate to="/" />
+}
+
+export default ProtectedRoute
